cb9fe6fa02f7ffff9a0c9a2738aaedfb92cd30fe
Soroush Rabiei <mohammadsoroushrabiei@gmail.com>
2026-07-28 11:40:30 +0330

    Implement button controlled LED toggle

--- main/main.c
+++ main/main.c
@@ -7,35 +7,57 @@
 #define BTN GPIO_NUM_1
 #define LED GPIO_NUM_2
 
+bool led_state = false;
+bool btn_last_state = false;
 
-// void btn_clicked(gpio_num_t GPIO_NUM)
-// {
 
-// }
+void led_init()
+{
+    gpio_reset_pin(LED);
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+}
+
+void btn_init()
+{
+    gpio_reset_pin(BTN);
+    gpio_set_direction(BTN, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BTN, GPIO_PULLDOWN_ONLY);
+}
+
+bool btn_is_pressed(gpio_num_t btn)
+{
+    bool btn_current_state = gpio_get_level(btn);
+        
+    if (btn_current_state != btn_last_state) {
+        vTaskDelay(pdMS_TO_TICKS(50)); // Debounce
+        btn_current_state = gpio_get_level(btn);
+        btn_last_state = btn_current_state ;
+
+        return btn_current_state;
+    }
+    
+    return false;
+}
+
+
+void led_toggle(gpio_num_t led)
+{
+    led_state = !led_state ;
+    gpio_set_level(led,led_state);
+}
 
 void app_main(void)
 {
-    gpio_reset_pin(LED);
-    gpio_reset_pin(BTN);
-    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
-    gpio_set_direction(BTN, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(BTN, GPIO_PULLDOWN_ONLY);
+    led_init();
+    btn_init();
+    
 
-    bool btn_last_state = false;
-    bool led_state = false;
     
     while (1)
     {
-        bool btn_current_state = gpio_get_level(BTN);
-        
-        if (btn_current_state != btn_last_state) {
-            vTaskDelay(pdMS_TO_TICKS(50)); // Debounce
-            btn_current_state = gpio_get_level(BTN);
-            if (btn_current_state == 1) {
-                led_state = !led_state;
-                gpio_set_level(LED, led_state);
-            }
-            btn_last_state = btn_current_state ;
+        if(btn_is_pressed(BTN))
+        {
+            led_toggle(LED);
         }
         
         vTaskDelay(pdMS_TO_TICKS(10));
